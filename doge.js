338ea8ae0af9c7376b1e431d76f412e5c8d96875
Brian de Heus <iambrian@docomo.ne.jp>
2013-12-16 19:53:49 +0900

    very commit. such first

---
+++ doge.js
@@ -0,0 +1,87 @@
+(function (window) {
+
+	var muchIndex    = 0;
+	var wowLength    = 0;
+	var manyWords    = null;
+	var suchInterval = null;
+	var muchPlay     = false;
+	var wowElement   = document.createElement('div');
+	var suchColors   = [
+		'#FF0000',
+		'#00FF00',
+		'#0000FF',
+	];
+
+	function veryRandom(val) {
+
+		return Math.floor((Math.random() * val));
+
+	}
+
+	function placeWord(word) {
+
+		var muchWidth  = window.innerWidth - 200; //Very random offset
+		var manyHeight = window.innerHeight - 26; //Such fontsize based offset
+
+		wowElement.textContent  = word;
+		wowElement.style.left   = veryRandom(muchWidth) + 'px';
+		wowElement.style.top    = veryRandom(manyHeight) + 'px';
+		wowElement.style.color  = suchColors[veryRandom(suchColors.length)];
+
+
+	}
+
+	function muchWords() {
+
+		muchPlay     = true;
+		suchInterval = setInterval(function () {
+
+			console.log('word:', manyWords[muchIndex]);
+
+			if(muchIndex === wowLength - 1) {
+				muchIndex = 0;
+			} else {
+				muchIndex++;
+			}
+
+			placeWord(manyWords[muchIndex]);
+
+		}, 1500);
+
+	}
+
+	var Doge = function (words) {
+
+		if (typeof(words) !== 'object' || words.length === undefined) {
+			return console.error('Wow. Words is not array. Much Error.');
+		}
+
+		if (words.length < 1) {
+			return console.error('Much dumb. Very fail. No words in array. Wow');
+		}
+
+		wowLength = words.length;
+		manyWords = words;
+
+		wowElement.style.position   = 'fixed';
+		wowElement.style.fontSize   = '26px';
+		wowElement.style.fontFamily = '"Comic Sans MS"';
+		document.body.appendChild(wowElement);
+		muchWords();
+
+	};
+
+	Doge.prototype.stop = function () {
+
+		if (muchPlay) {
+
+			muchPlay = false;
+			clearInterval(suchInterval);
+
+		}
+
+	};
+
+	window.Doge = Doge;
+
+}(window));
